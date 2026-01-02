c6d608d125daa2169ddb984e041a5437a62274cd
fish-gelly <fishgelly86@gmail.com>
2026-01-02 15:01:27 +0100

    fix: enable PWA default and link manifest

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   images: {
@@ -28,4 +32,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
